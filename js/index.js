919d4b03b27d44cc4c22e7e03ad74d6e0db87bee
Glory Katende <katendeglory@gmail.com>
2020-04-29 14:35:25 +0200

    initial commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const root = document.documentElement;
+const toggler = document.getElementById('mode-toggler');
+
+toggler.addEventListener('change', e => {
+  root.style.setProperty('--transition', "left .5s");
+  if (e.target.checked) document.body.classList.replace('light-mode', 'dark-mode');
+  else document.body.classList.replace('dark-mode', 'light-mode');
+  setTimeout(() => root.style.setProperty('--transition', "all 0.25s ease-in-out"), 1000);
+});
