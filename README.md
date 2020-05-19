095e6e11f792a9aa2e0ecfe98c37e2c6cfa260ee
Glory Katende <katendeglory@gmail.com>
2020-05-19 12:55:53 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 
 [📚 Documentation](https://katendeglory.github.io/soft-ui-library/) 
+
+[📚 NPM Package](https://www.npmjs.com/package/soft-ui-library) 
--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const toggler = document.getElementById('mode-toggler');
 
 toggler.addEventListener('change', e => {
   root.style.setProperty('--transition', "left .5s");
-  if (e.target.checked) document.body.classList.replace('light-mode', 'dark-mode');
+  if (!e.target.checked) document.body.classList.replace('light-mode', 'dark-mode');
   else document.body.classList.replace('dark-mode', 'light-mode');
   setTimeout(() => root.style.setProperty('--transition', "all 0.25s ease-in-out"), 1000);
 });
